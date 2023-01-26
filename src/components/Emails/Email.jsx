d81dd91dc7f8c078f3c3e56a49c4fb8668e4cac8
Damar Buana Murti <damarbm30@gmail.com>
2023-01-26 21:45:49 +0700

    sort descending

--- src/components/Emails/Email.jsx
+++ src/components/Emails/Email.jsx
@@ -1,12 +1,4 @@
-import { ListItem, ListItemButton, ListItemText } from "@mui/material";
-
 const Email = ({ email }) => {
-  return (
-    <ListItem disablePadding>
-      <ListItemButton>
-        <ListItemText primary={email} />
-      </ListItemButton>
-    </ListItem>
-  );
+  return <p>{email}</p>;
 };
 export default Email;
--- src/components/Emails/Emails.jsx
+++ src/components/Emails/Emails.jsx
@@ -1,17 +1,35 @@
-import { Box, List } from "@mui/material";
-
+import { TableContainer, TableBody, TableCell, Table, TableHead, TableRow } from "@mui/material";
 import Email from "./Email";
 
 const Emails = ({ users }) => {
+  const sortedEmailDesc = users.sort((a, b) => {
+    const second = a.email.toLowerCase();
+    const first = b.email.toLowerCase();
+
+    if (second > first) {
+      return -1;
+    } else if (second < first) {
+      return 1;
+    }
+
+    return 0;
+  });
+
   return (
-    <Box>
-      <h1>List of Emails</h1>
-      <List>
-        {users.map((user) => {
-          return <Email key={user.id} email={user.email} />;
-        })}
-      </List>
-    </Box>
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Email (Descending)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {sortedEmailDesc.map((email) => {
+            return <Email key={email.id} email={email.email} />;
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 export default Emails;
